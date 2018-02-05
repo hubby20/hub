165b3adb49a1151bfb25b5b8bcdf0ee50473d818
hubby20 <36042302+hubby20@users.noreply.github.com>
2018-02-05 20:24:36 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+h1{
+   color:blue;
+   font-family:anaphora;
+   text-align:center;
+} 
+p1{
+   color:yellow;
+   font-family:arial;
+}
+h2{ 
+
+   color:green;
+   text-align:center;
+   font-family:verdana;
+}
+p2{
+   color:orange;
+
+</style>
+</head>
+<body style="background-color:powderblure;">
+<h1>Basic Information</h1><p1>Name:Mancao,Geneva S.<br>Address:Pitalo,San Fernando,Cebu<br>
+Contact:09750673174<br>Email:geneva.mancao@gmail.com<br>
+Age:24 years old<br>DOB:May 6,1993</p1><h2>Biography</h2>
+<p2><center>Every success is built on to do ability to do better then good enough. </center>
+Im Geneva S.Mancao.24 years old I live in Pitalo San Fernando,Cebu.I have two brothers and two sisters, Im the fourth among them.
+I study at Cebu Technological University.My fathers name is Felix O. Mancao he is a foreman carpenter.
+My mothers name is Elvira S.Mancao she is a plain housewife.
+My greatest fear is losing my love ones specially my family.
+My goal is to finish my study and to have a stable job in abroad.</p2>
+
+
+</body>
+</html>
